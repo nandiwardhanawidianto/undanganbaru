26aa7ecfbb6cbb0dc8b5af861f89bbe6f7c5144b
nandi <nandib02@gmail.com>
2026-07-21 14:56:47 +0700

    minor bug api

--- src/hooks/useInvitationData.js
+++ src/hooks/useInvitationData.js
@@ -16,7 +16,7 @@ export default function useInvitationData() {
       try {
         setLoading(true);
         setError(null);
-        const API_URL = `http://127.0.0.1:8000/api/slug/${slug}/listapi`;
+        const API_URL = `http://cms.royalweddinginvitiation.com/api/slug/${slug}/listapi`;
         const response = await fetch(API_URL, {
           headers: {
             Accept: "application/json",
