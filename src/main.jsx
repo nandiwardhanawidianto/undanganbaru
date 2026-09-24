2f4cb27a39f46399d4c265f6c8c44a745621d487
nandiwardhanawidianto <62790811+nandiwardhanawidianto@users.noreply.github.com>
2026-09-25 03:53:36 +0700

    Route Hitam2 preview directly to CMS fetcher

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import Hitam2Preview from './Hitam2Preview.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/preview/hitam2/:slugId" element={<App previewTheme="hitam2" />} />
+        <Route path="/preview/hitam2/:slugId" element={<Hitam2Preview />} />
         <Route path="/:slugId" element={<App />} />
       </Routes>
     </BrowserRouter>
