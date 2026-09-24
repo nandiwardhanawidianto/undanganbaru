3b778efe3d52c268fbca47f95f257bf28821407b
nandiwardhanawidianto <62790811+nandiwardhanawidianto@users.noreply.github.com>
2026-09-25 03:54:28 +0700

    Restore normal invitation slug routing for Hitam2

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import Hitam2Preview from './Hitam2Preview.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/preview/hitam2/:slugId" element={<Hitam2Preview />} />
         <Route path="/:slugId" element={<App />} />
       </Routes>
     </BrowserRouter>
